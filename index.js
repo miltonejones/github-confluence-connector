37de3edf78f2064a6f86a7bd09ec7b77665e23cb
miltonejones <miltonejones@gmail.com>
2022-08-18 05:58:52 -0500

    My first action is ready

---
+++ index.js
@@ -0,0 +1,69 @@
+const request = require("request");
+const core = require('@actions/core');
+const github = require('@actions/github');
+ 
+const createConfluencePage = ({ spacekey, pageTitle, pageContent, username, password, endpoint }) =>
+ new Promise((resolve, reject) => { 
+   const url = `https://${endpoint}/wiki/rest/api/content`; 
+   const auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+  
+   // build Confluence request
+   const body = {
+    type: "page",
+    title: pageTitle,
+    space: {
+      key: spacekey
+    },
+    body: {
+      wiki: {
+        value: pageContent,
+        representation: "wiki"
+      }
+    }
+  };
+
+  // send request
+  request({
+       url,
+       method: "POST",
+       body: JSON.stringify(body),
+       headers: {
+         Authorization: auth,
+         "Content-Type": "application/json",
+       },
+     },
+     function (error, response, body) {
+       if (error) {
+         return reject(error);
+       }
+       resolve(body); 
+     }
+   );
+ });
+
+
+
+try { 
+  const endpoint = core.getInput('endpoint'); 
+  const username = core.getInput('username'); 
+  const password = core.getInput('password'); 
+  const spacekey = core.getInput('spacekey'); 
+  const { author, message: pageContent } = github.context.payload.head_commit;
+  const pageTitle = `Created by ${author.name}`;
+
+  createConfluencePage({
+    pageTitle,
+    pageContent,
+    endpoint,
+    username,
+    password,
+    spacekey,
+  }).then(response => {
+    console.log ('success', response);
+    const time = (new Date()).toTimeString();
+    core.setOutput("message", 'Page created on ' + time);
+  });
+ 
+} catch (error) {
+  core.setFailed(error.message);
+}
